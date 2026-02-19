e93bd4ce3e346149880d9722341dad32b0ca4be2
Gokul B <lokeshlokesh91713@gmail.com>
2026-02-19 10:50:48 +0530

    Delete project directory

--- project/generate_hashes.php
+++
@@ -1,5 +0,0 @@
-<?php
-// generate_hashes.php
-echo "admin123 hash: " . password_hash('admin123', PASSWORD_DEFAULT) . "<br>";
-echo "1234 hash: " . password_hash('1234', PASSWORD_DEFAULT) . "<br>";
-?>
